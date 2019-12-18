b0f0a44e98b267a6dbe15d98f2b28dcf4cd58513
Samridh <samridhharsh@gmail.com>
2019-12-18 15:16:37 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -22,13 +22,26 @@ class App extends React.Component {
 
   initMap = async (lat, lon) => {
     await this.setState({ lat, lon });
+    
+    const center = { lat: this.state.lon, lng: this.state.lat };
+    console.log(center);
+    
     if (this.state.lat && this.state.lon) {
-      const center = { lat: this.state.lon, lng: this.state.lat };
-      console.log(center);
-      new window.google.maps.Map(document.getElementById("map"), {
+
+      
+      const map = new window.google.maps.Map(document.getElementById("map"), {
         center: center,
         zoom: 3
       });
+
+      var marker = new window.google.maps.Marker({
+        position: center,
+        title: "Hello World!"
+      });
+
+      // To add the marker to the map, call setMap();
+      marker.setMap(map);
+
     } else {
       new window.google.maps.Map(document.getElementById("map"), {
         center: { lat: 12.972442, lng: 77.580643 },
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,28 @@
 import React from "react";
 
 import { Form, Button, FormGroup } from "reactstrap";
+// import autosearch from '/public/autosearch';
 
 class Formclass extends React.Component {
+
+  handleChange = event => {
+    console.log(event.target.value);
+    
+  };
+
   render() {
     return (
       <div className="form-container">
         <Form onSubmit={this.props.GetWeather} className="form">
           <FormGroup>
-            <input type="text" name="city" placeholder="City" required/>
+          <input type="text" name="city" placeholder="city" required />
           </FormGroup>
           <FormGroup>
-            <input type="text" name="country" placeholder="Country" required/>
+            <input type="text" name="country" placeholder="Country" required />
           </FormGroup>
-            <Button color="info" type="submit">
-              <span> Get Weather...</span>
-            </Button>
+          <Button color="info" type="submit">
+            <span> Get Weather...</span>
+          </Button>
         </Form>
       </div>
     );
